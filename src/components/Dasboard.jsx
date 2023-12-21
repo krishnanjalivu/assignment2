b6a8ce0bce64997dd83b92640a231fa58bb74100
krishnanjalivu <vukrishnanjali@gmail.com>
2023-12-21 12:08:30 +0530

    first commit

--- src/components/Dasboard.jsx
+++ src/components/Dasboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 
+
+
 function Dashboard() {
   return (
     <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Home from "../assets/home (1).png";
+import User from "../assets/user (1).png";
+import Profile from "../assets/Group 46@2x.png";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
 
-      <Link to="/dashboard"><img className='home' src="/src/assets/home (1).png" height={25} width={25}></img><span className='link'>Home</span></Link>
-      <Link to="/employees"><img className='home' src="/src/assets/user (1).png" height={25} width={25}></img><span className='link'>Employees</span></Link>
-      <img className='profile' src="/src/assets/Group 46@2x.png" alt='profile' height={60} width={60}></img>
+      <Link to="/dashboard"><img className='home' src={Home} height={25} width={25}></img><span className='link'>Home</span></Link>
+      <Link to="/employees"><img className='home' src={User} height={25} width={25}></img><span className='link'>Employees</span></Link>
+      <img className='profile' src={Profile} alt='profile' height={60} width={60}></img>
     </div>
   )
 }
